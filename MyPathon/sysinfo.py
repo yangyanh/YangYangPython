5db291519ec23160fcc6427150f0679b33b25fe5
yangyanhua <806917822@qq.com>
2019-12-02 22:24:06 +0800

    a

--- MyPathon/sysinfo.py
+++ MyPathon/sysinfo.py
@@ -1,6 +1,14 @@
 import sys
+import time
+def printTime():
+    time_format= '%Y-%m-%d %X'
+    time_content = time.strftime(time_format)
+    print(time_content)
+    time_result =time.strptime(time_content,time_format)
+    print(time_result)
 def printsysParam():
     print(sys.version_info)
 
 
 printsysParam()
+printTime()
